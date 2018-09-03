910a66ac4f692f4fe22d023703bd313ff054a761
Stephanie <smedina7@miners.utep.edu>
2018-09-03 15:31:28 -0600

    modified code

--- wordCount.py
+++ wordCount.py
@@ -1,16 +1,21 @@
+# Open input text file
 file = open('declaration.txt', 'r')
-book = file.read()
+readFile = file.read()
+
+# Open output text file
+fileOut = open('outputCount.txt', 'w')
 
 
+# Method to tokenize words in the text file
 def tokenize():
     if book is not None:
-        words = book.split()
+        words = book.lower().split()
         return words
     else:
         return None
         
 
-def map_book(tokens):
+def map_text(tokens):
     hash_map = {}
 
     if tokens is not None:
@@ -19,7 +24,7 @@ def map_book(tokens):
             word = element.replace(",","")
             word = word.replace(".","")
 
-            # Word Exist?
+            # Does the word appear in list?
             if word in hash_map:
                 hash_map[word] = hash_map[word] + 1
             else:
@@ -29,14 +34,20 @@ def map_book(tokens):
     else:
         return None
 
+#Open output file
+fileOut = open('outputCount.txt', 'w')
 
 # Tokenize the Book
 words = tokenize()
 #word_list = ['HE','Multitude']
 
 # Create a Hash Map (Dictionary)
-map = map_book(words)
+map = map_text(words)
 
 # Show Word Information
-#for word in word_list:
-#    print('Word: [' + word + '] Frequency: ' + str(map[word]))
+for word in sorted(map):
+    fileOut.write(word+ " " + str(map[word]) + "\n")
+
+# Close files
+file.close()
+fileOut.close()
