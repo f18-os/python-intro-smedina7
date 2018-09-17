3abac24bfd62b8858a417b720f3c23e25890d93a
Stephanie <smedina7@miners.utep.edu>
2018-09-17 15:39:02 -0600

    Adding Redirection Fix

--- Shell.py
+++ Shell.py
@@ -5,39 +5,22 @@ import sys
 import time
 import os
 
-pid = os.getpid()               # get and remember pid
-
-os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
-
-rc = os.fork()
-
-#Store user input
-userCommand = input("Input command: ")
-inputCommand = re.split('\s+', userCommand)
-print(inputCommand)
-
-command = None
-argI = None
+#command = None
 file = None
+userCommand = ""
+args = []
 
-while(inputCommand !=exit):
-    if '>' in inputCommand:
-        #runs until it has read all the words
-        #store in format for '>'
-        # command args > file
-        command = inputCommand[0]
-        #print("Command val: " + command)
-        argI = inputCommand[1]
-        #print("Args val: " + argI)
-        file = inputCommand[3]
-        #print("File val: " + file)
-    elif '<' in inputCommand:
-        #store in format for '<'
-        # file < command args
-        file = inputCommand[0]
-        command = inputCommand[2]
-        argI = inputCommand[3]
+while(userCommand != "exit"):
+
+    #Store user input
+    userCommand = input("steph-shell$ ")
+    args = userCommand.split()
     
+
+    pid = os.getpid()               # get and remember pid
+    os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
+    rc = os.fork()
+
     #Determine what commands the user wants to run
     if rc < 0:
         os.write(2, ("fork failed, returning %d\n" % rc).encode())
@@ -45,28 +28,43 @@ while(inputCommand !=exit):
     elif rc == 0:                   # child
         os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" %
                      (os.getpid(), pid)).encode())
-        args = [command, argI]
-        os.close(1)                 # redirect child's stdout
-        sys.stdout = open(file, "w")
-        fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
-        os.set_inheritable(fd, True)
-        os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
+        if '>' in userCommand:
+            inputR = args.index('>')  #store index of '>'
+            file = args[3]
+            args = args[:inputR]
+            os.close(1)                  #redirect child's stdout
+            sys.stdout = open(file, "w")
+            fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
+            os.set_inheritable(fd, True)
+            os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
+ #           sys.exit(1)
+        elif '<' in userCommand:
+            outputR = args.index('<')   #store index of '<'
+            args =  args[:outputR] + args[outputR+1:]
+ #           sys.exit(1)
         
+ #       elif userCommand != "exit":
+            
+            #sys.exit(1)
+             
+    
         for dir in re.split(":", os.environ['PATH']): # try each directory in path
             program = "%s/%s" % (dir, args[0])
             try:
                 os.execve(program, args, os.environ) # try to exec program
             except FileNotFoundError:             # ...expected
-                pass                              # ...fail quietly 
+                pass                              # ...fail quietly
 
         os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
-        sys.exit(1)                 # terminate with error
+        os.write(1, ("Command not found, try another command!").encode())
+        
+ #       sys.exit(1)                 # terminate with error
 
     else:                           # parent (forked ok)
-        os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % 
+        os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" %
                  (pid, rc)).encode())
         childPidCode = os.wait()
-        os.write(1, ("Parent: Child %d terminated with exit code %d\n" % 
+        os.write(1, ("Parent: Child %d terminated with exit code %d\n" %
                  childPidCode).encode())
 sys.exit(1)
 print("Closing shell. Good-Bye.")
