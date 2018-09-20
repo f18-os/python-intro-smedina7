1f5cb1f6482afeb67eca45a2b1c460bc31602b6e
Stephanie <smedina7@miners.utep.edu>
2018-09-19 22:00:08 -0600

    Cleaning code and trying to implement piping

--- Shell/Shell.py
+++ Shell/Shell.py
@@ -4,14 +4,16 @@ import re
 import sys
 import time
 import os
+import fileinput
 
 #command = None
 file = None
 userCommand = ""
 args = []
+commandFound = True
 
 try:
-    while(userCommand != "exit"):
+    while(userCommand != "exit"):  #2
         try:
             envCommand = os.environ['ps1']
             
@@ -22,16 +24,21 @@ try:
         if osEn is False:
             envCommand = ("$ ")
 
-    #Store user input
- #   userCommand = input("steph-shell$ ")
+      #Store user input
+    #   userCommand = input("steph-shell$ ")
         userCommand = input(envCommand)
         #print(userCommand)
         args = userCommand.split(" ")
         #print(args)
         
+        #for pipe
+        pr,pw = os.pipe()
+        for f in (pr, pw):
+            os.set_inheritable(f, True)
+        
         if 'cd' in userCommand:
             inputR = args.index('cd')
-            newDir = args[inputR+1:]
+            newDir = args[inputR+1:]  #1
             try:
                 os.chdir(os.path.expanduser(newDir[0]))
                 
@@ -59,19 +66,41 @@ try:
                 
             elif '<' in userCommand:
                 outputR = args.index('<')   #store index of '<'
+                
+                
+                #in case file that is being read has an exec
+##                openF = open(args[-1], "r")
+##                openOS = os.open(args[-1], os.O_RDONLY)  #6
+##                fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
+##                dup2(openOS, fd)
+##                
                 args =  args[:outputR] + args[outputR+1:]
                 
             elif 'echo' in userCommand:
+                #2
                 echoCommand = userCommand.replace(userCommand[:5], '')
                 args = ["echo", echoCommand]
                 
             elif len(userCommand) == 2:
+                #2
                 userInput = userCommand.split(" ")
                 args = [userInput[0]]
             
             elif 'cd' in userCommand:
                 inputR = args.index('cd')
                 newDir = args[inputR+1:]
+                
+            elif '|' in userCommand:               
+                os.close(1)                 # redirect child's stdout
+                os.dup(pw)
+                for fd in (pr, pw):
+                    os.close(fd)
+                
+                length =len(args)-1
+                inputR = args.index('|')
+                
+                args = [args[:outputR], args[outputR+1:]]
+                    
             
             for dir in re.split(":", os.environ['PATH']): # try each directory in path
                 program = "%s/%s" % (dir, args[0])
@@ -79,13 +108,24 @@ try:
                 try:
                     os.execve(program, args, os.environ) # try to exec program
                 except FileNotFoundError:             # ...expected
+                    commandFound = False
+                    #pass                              # ...fail quietly
+                
+            if commandFound is False:
+                if userCommand != 'exit':
                     os.write(2, ("Command not found, try another command!\n").encode())
-                    pass                              # ...fail quietly
+            
         else:                           # parent (forked ok)
+            if '|' in userCommand:
+                os.close(0)
+                os.dup(pr)
+                for fd in (pw, pr):
+                    os.close(fd)
             childPidCode = os.wait()
-        
-    sys.exit(0)
+    
+    sys.exit(1)
+    print("Closing shell. Good-Bye.")
             
 except EOFError:
     sys.exit(0)
-    print("Closing shell. Good-Bye.")
+    print("Closing shell. Good-Bye.") #2
